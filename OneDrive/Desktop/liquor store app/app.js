c9093df00f1e1515ca62770fdd5b0906c73cabe4
Brian Kimani <kimanibrian439@gmail.com>
2025-06-13 11:07:52 +0300

    Add sample liquor products data

---
+++ OneDrive/Desktop/liquor store app/app.js
@@ -0,0 +1,27 @@
+// data/products.js
+
+const products = [
+  {
+    id: 1,
+    name: "Hennessy VS",
+    type: "Cognac",
+    price: 55.99,
+    inStock: true,
+  },
+  {
+    id: 2,
+    name: "Ciroc Peach",
+    type: "Vodka",
+    price: 34.99,
+    inStock: true,
+  },
+  {
+    id: 3,
+    name: "Don Julio 1942",
+    type: "Tequila",
+    price: 149.99,
+    inStock: false,
+  },
+];
+
+export default products;
